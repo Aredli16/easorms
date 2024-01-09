164a13593b2c7267e79582df2ab93b70ac490eae
Lempereur Corentin <73390812+aredli16@users.noreply.github.com>
2024-01-10 00:08:55 +0100

    Launch app fix

--- front/src/lib/client/school-year.ts
+++ front/src/lib/client/school-year.ts
@@ -1,7 +1,7 @@
 import { getAccessToken } from '@/lib/client/access_token';
 
 export const setCurrentSchoolYear = async (id: string) => {
-  const response = await fetch(`${process.env.API_URL}/school-year/current/${id}`, {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/school-year/current/${id}`, {
     method: 'POST',
     headers: {
       Authorization: `Bearer ${await getAccessToken()}`,
